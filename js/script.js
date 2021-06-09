6007b3c5c90011d19a8d08cf13445d5357272e7b
Babatunde Baruwa <babatundesbaruwa@gmail.com>
2021-06-09 17:43:35 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Javascript Document
+// swiper script
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5.2,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
